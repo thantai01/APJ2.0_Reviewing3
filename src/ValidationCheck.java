e9dd2c1aaa13a7c029d8890c6772cac687e0d322
thantai01 <thantaihp2011@gmail.com>
2021-06-03 11:57:48 +0700

    ...

---
+++ src/ValidationCheck.java
@@ -0,0 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidationCheck {
+    private static Pattern pattern;
+    private Matcher matcher;
+    private static final String STUDENT_ID_REGEX  = "";
+    private static final String STUDENT_EMAIL_REGEX = "";
+    private static final String STUDENT_DATE_REGEX = "";
+
+    public ValidationCheck() {
+
+    }
+}
